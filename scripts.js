7a39f4a3e16f94b245cb7c9adb1f313dcc9d9ad5
Noahs-Wax-N-Wrench <nkristensen77@gmail.com>
2025-07-29 23:41:27 -0500

    Update scripts.js
    Signed-off-by: Noahs-Wax-N-Wrench <nkristensen77@gmail.com>

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const images = [
   "Pictures/Lexus-Back-Carpet-Before-And-After.jpg",
   "Pictures/Lexus-Front-Carpet-Before-And-After.jpg",
   "Pictures/Lexus-Back-Stripes-One.jpg",
-  "Pictures/Lexus-Front-Driver-And-Passenger.jpg",
+  "Pictures/Lexus-Front-Driver-And-Passenger-One.jpg",
   "Pictures/Clear-Coat-Scratch-Before-and-After.jpeg",
   "Pictures/Kia-Front-Before-and-After.jpeg",
   "Pictures/Kia-Backseat-Before-and-After.jpeg",
